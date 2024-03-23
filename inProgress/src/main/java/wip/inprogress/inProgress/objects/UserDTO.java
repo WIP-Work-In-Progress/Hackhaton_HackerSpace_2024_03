86e0c9e022fc48d0ef016225af008674407aae82
Dawid Roszman <dawidroszman@gmail.com>
2024-03-23 09:00:57 +0100

    updated userDTO

--- inProgress/src/main/java/wip/inprogress/inProgress/objects/UserDTO.java
+++ inProgress/src/main/java/wip/inprogress/inProgress/objects/UserDTO.java
@@ -1,10 +1,14 @@
 package wip.inprogress.inProgress.objects;
 
+import wip.inprogress.inProgress.models.Experience;
 import wip.inprogress.inProgress.models.UserEntity;
 
+import java.util.List;
+import java.util.stream.Collectors;
 
-public record UserDTO(String firstName, String lastName) {
+
+public record UserDTO(String firstName, String lastName, Integer age, List<String> experiences){
     public static UserDTO from(UserEntity user) {
-        return new UserDTO(user.getFirstName(), user.getLastName());
+        return new UserDTO(user.getFirstName(), user.getLastName(), user.getAge(), user.getExperiences().stream().map(Experience::getName).collect(Collectors.toList()));
     }
 }
